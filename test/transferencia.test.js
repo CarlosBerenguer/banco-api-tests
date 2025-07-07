80390113b540f2f0050b61b3be0cda667e49ff89
CarlosBerenguer <carloshfb03@gmail.com>
2025-07-07 17:19:39 -0300

    Adicionando hook de beforeEach para captura do token

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,10 +5,15 @@ require('dotenv').config()
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
-        it('Deve retornar sucesso com 201 quando o valor da transferencia igual ou acima de 10 reais', async () => {
-            //capturar TOKEN
-            const token = await obterToken('julio.lima', '123456')
+        
+        let token;
 
+        beforeEach(async () => {
+            token = await obterToken('julio.lima', '123456')
+        })
+        
+        it('Deve retornar sucesso com 201 quando o valor da transferencia igual ou acima de 10 reais', async () => {
+            
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
@@ -25,8 +30,6 @@ describe('Transferencias', () => {
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo e 10 reais', async () => {
 
-            const token = await obterToken('julio.lima', '123456')
-
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
@@ -42,8 +45,6 @@ describe('Transferencias', () => {
         })
 
         it('Deve retornar sucesso com 201 quando o valor da transferencia for acima de 5000 e contiver o token', async () => {
-
-            const token = await obterToken('julio.lima', '123456')
 
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
